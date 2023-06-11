3670abdd48d18f579405e1738f7aa9d850e95680
Rohit Sarkar <rohitsarkar034@gmail.com>
2023-06-11 10:51:59 +0530

    Creating Login, Clear and Signup buttons and implementing the ActionListener interface and declaring the previously mentioned buttons as global and adding the ActionPerformed method to override the default one provided with the interface, adding condition to clear the textfields as soon as the clear button is pressed

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -1,11 +1,14 @@
 package bank.management.system;
 
+import java.awt.event.*;
 import  java.util.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class Login extends JFrame {
-    Login(){
+public class Login extends JFrame implements ActionListener {
+   JButton login_button, clear_button, signup_button;
+   JTextField first_textfield, second_textfield;
+   Login(){
 //The next 3 lines are to be written first as if it is not done, the image is not loaded.
         ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("icons/Bank_image.jpg"));
         Image myImage = icon.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
@@ -16,6 +19,23 @@ public class Login extends JFrame {
         myLabel.setBounds(70, 20, 240, 150);
         add(myLabel);
 //To create buttons, we can use the JButton class in Java
+        login_button = new JButton("LOGIN");
+        login_button.setBackground(Color.LIGHT_GRAY);
+        login_button.setBounds(450, 300, 80, 30);
+        login_button.addActionListener(this);
+        add(login_button);
+
+        clear_button = new JButton("CLEAR");
+        clear_button.setBackground(Color.LIGHT_GRAY);
+        clear_button.setBounds(570, 300, 80, 30);
+        clear_button.addActionListener(this);
+        add(clear_button);
+
+        signup_button = new JButton("SIGN UP");
+        signup_button.setBackground(Color.LIGHT_GRAY);
+        signup_button.setBounds(500, 350, 100, 30);
+        signup_button.addActionListener(this);
+        add(signup_button);
 
         getContentPane().setBackground(Color.WHITE);
 //We can place an ImageIcon inside Jlabel but we cant place an image and to solve this problem, we convert an image into an imageIcon
@@ -30,16 +50,16 @@ public class Login extends JFrame {
 
         JLabel cardno_label = new JLabel("Card number: ");
         cardno_label.setBounds(150, 200, 200, 40);
-        JTextField first_textfield = new JTextField("Enter the card number ");
+        first_textfield = new JTextField("Enter the CARD Number");
         first_textfield.setBounds(450,200, 200, 40);
         first_textfield.setVisible(true);
         cardno_label.setFont(myFont2);
         add(cardno_label);
         add(first_textfield);
 
-        JLabel pinno_label = new JLabel("Enter your PIN number: ");
+        JLabel pinno_label = new JLabel("PIN number: ");
         pinno_label.setBounds(150, 240, 320, 40);
-        JTextField second_textfield = new JTextField("Enter the PIN number ");
+        second_textfield = new JTextField("Enter the PIN Number");
         second_textfield.setBounds(450, 250, 200, 40);
         second_textfield.setVisible(true);
         pinno_label.setFont(myFont2);
@@ -47,14 +67,29 @@ public class Login extends JFrame {
         add(second_textfield);
 
         setLayout(null);
-        setSize(800, 400);
+        setSize(800, 450);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocation(150, 70);
         setTitle("ATM Machine");
     }
+
+   @Override
+   public void actionPerformed(ActionEvent ae){
+        if (ae.getSource() == login_button){
+
+        }
+        if (ae.getSource() == clear_button){
+             first_textfield.setText("");
+             second_textfield.setText("");
+        }
+        if (ae.getSource() == signup_button){}
+   }
+
     public static void main(String[] args) {
 
         new Login(); //creating an object of class
     }
+
+
 }
